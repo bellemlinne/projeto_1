eed0d1d48244cad11c0207503d599856d5abe0c4
bellemlinne <alinebelemtavares@gmail.com>
2021-09-16 16:36:22 -0300

    any_many

--- R/2-Scripts_ENMs_SDMs/1-setup_sdmdata.R
+++ R/2-Scripts_ENMs_SDMs/1-setup_sdmdata.R
@@ -16,13 +16,21 @@ library(raster)
 
 ### Importando e lendo sua planilha no ambiente R. read.csv é uma função para ler a extensão .csv. NO argumento "file" coloque o caminho relativo do arquivo .csv , no arquivo "sep" indique qual o tipo de separado dos campos (o que separa as colunas).
 
-sp_input <- read.csv(file = "./dados/ocorrencias/sp_input_setupsdmdata.csv", sep = ",") # 
+sp_input <- read.csv(file = "./dados/ocorrencias/sp_input_setupsdmdata.csv", sep = ",") 
+
+## Colocando no formato exigido pelo pacote: species name separated by "_" 
+sp_input$species <-
+  gsub(x = sp_input$species,
+       pattern = " ",
+       replacement = "_") 
+
 
 ## Carregando as variáveis ambientais
 
-> lista_arquivos <- list.files("./dados/raster/variaveis_cortadas_pratica/", full.names = T, pattern = ".tif")
+lista_arquivos <- list.files("./dados/raster/variaveis_cortadas_pratica/", full.names = T, pattern = ".tif")
 
 vars_stack <-stack(lista_arquivos)
+plot(vars_stack[[12]])
 
 ## Verificando os pontos nas variáveis mais um vez. Isso pode ser feito previamente no Qgis. Em termos de verificação de pontos, ou verificar valores de pixel de forma mais rápida, o Qgis pode ser mais apropriado. 
 ## Talvez não seja possível gerar a imagem e código abaixo não funcione a cuse um erro. Por isso é aconselhável que seja feita a verificação dos pontos em cimas das camadas ambientais no Qgis.
@@ -54,7 +62,7 @@ setup_sdmdata_1 <- setup_sdmdata(species_name = unique(sp_input[1]),
               clean_uni = TRUE,
               clean_nas = TRUE,
               #geo_filt = FALSE,
-              #geo_filt_dist = 10,
+              #geo_filt_dist = 0,
               select_variables = TRUE,
               sample_proportion = 0.5,
               cutoff = 0.7)
